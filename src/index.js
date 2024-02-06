a5487b149c87fdd4683c562af24ea63362f590ed
Nazligül Aydin <naydinhakzell.schueler.at>
2024-02-06 16:53:08 +0100

    add background

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import hintergrund from './assets/hintergrund.png';
+import unten from './assets/unten.png';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
+class MyGame extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MyGame' });
     }
 
-    preload ()
-    {
-        //  This is an example of a bundled image:
-        this.load.image('logo', logoImg);
-
-        //  This is an example of loading a static image from the public folder:
-        this.load.image('background', 'assets/bg.jpg');
+    preload() {
+        this.load.image('backgroundImg', hintergrund);
+        this.load.image('barBottom', unten);
     }
-      
-    create ()
-    {
-        this.add.image(400, 300, 'background');
 
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+    create() {
+        const background = this.add.image(0, 0, 'backgroundImg').setOrigin(0);
+
+        // Skaliere das Hintergrundbild, um das gesamte Fenster zu füllen
+        background.setScale(this.game.canvas.width / background.width, this.game.canvas.height / background.height);
+
+        const barGroup = this.add.group(); // Erstelle eine normale Gruppe für die Balken
+
+        // Erstelle Balken unten
+        const barSpacing = 100; // Abstand zwischen den Balken
+        const barWidth = 40; // Breite der Balken
+        const numBars = Math.floor(800 / (barWidth + barSpacing));
+        const yValues = []; // Array für die zufälligen Y-Positionen der Balken
+
+        for (let i = 0; i < numBars; i++) {
+            yValues.push(Phaser.Math.Between(50, 300)); // Füge eine zufällige Y-Position zum Array hinzu
+        }
+
+        for (let i = 0; i < numBars; i++) {
+            const x = (i * (barWidth + barSpacing)) + (barWidth / 2);
+            const y = yValues[i]; // Holen Sie sich die Y-Position aus dem Array
+            const barBottom = barGroup.create(x, y, 'barBottom').setOrigin(0.5, 0);
+        }
     }
 }
 
@@ -39,7 +43,7 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
+    scene: [MyGame]
 };
 
 const game = new Phaser.Game(config);
